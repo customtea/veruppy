5c164070cf9568df5c11f727d32277511cf040a0
customtea <kon.taku8@gmail.com>
2022-09-27 13:41:06 +0900

    fix; int cast exception

--- verup.py
+++ verup.py
@@ -4,27 +4,27 @@ import sys
 import glob
 import os
 
-def version_up(filefullpath: str):
-    filedirname = os.path.dirname(filefullpath)
-    filepath, extension = os.path.splitext(filefullpath)
-    filename = os.path.basename(filepath)
+def version_up(filefullname: str):
+    filefullname = os.path.basename(filefullname)
+    filename = filefullname.split(".")[0]
+    extension = filefullname.split(".")[1]
     # get file list
-    filelist = glob.glob(f"{filedirname}/*{extension}")
+    filelist = glob.glob(f"*.{extension}")
     # filtered filename included
     filelist = list(filter(lambda x:filename in x, filelist))
     # filtered version num (_vXX) included
     filelist = list(filter(lambda x:"_v" in x, filelist))
     if len(filelist) != 0:
-        filelist = sorted(filelist, key=lambda x: int(x.split("_")[-1].split(".")[0][1:]), reverse=True)
+        filelist = sorted(filelist, key=lambda x: int(x.split("_")[-1].split(".")[0][1:]) if x.split("_")[-1].split(".")[0][1:].isnumeric() else -1, reverse=True)
         vernum = int(filelist[0].split("_")[-1].split(".")[0][1:])
         newvernum = vernum + 1
     else:
         newvernum = 1
 
     newvernumchar = format(newvernum, "02d")
-    newfilename = f"{filedirname}/{filename}_v{newvernumchar}{extension}"
-    shutil.copy(filefullpath, newfilename)
-    print(f"{filefullpath} is copied {newfilename}")
+    newfilename = f"{filename}_v{newvernumchar}.{extension}"
+    shutil.copy(filefullname, newfilename)
+    print(f"{filefullname} is copied {newfilename}")
 
 
 if __name__ == '__main__':
